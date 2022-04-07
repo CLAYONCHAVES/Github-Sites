199d2a2f5876699fbe0f4da2d5dd068b3df7cf83
CLAYONCHAVES <98711507+CLAYONCHAVES@users.noreply.github.com>
2022-04-07 15:30:23 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+//inserindo um texto==>muitostextos /*/*
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
+//alerta sinlencioso
+console.log('Oi isso e um console.log /alerta silencioso!/');
+//Declaração//
+var a=1;b=6;c=a+b;
+console.log(c); 
+//var,let e const
+
+
